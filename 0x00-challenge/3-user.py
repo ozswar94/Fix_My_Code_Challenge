197adb30fd5d4a8f543a184d79880393a7d0713d
ozswar94 <3630@holbertonschool.com>
2022-04-24 14:48:29 +0000

    fix 3

--- 0x00-challenge/3-user.py
+++ 0x00-challenge/3-user.py
@@ -50,6 +50,8 @@ class User():
         - `False` if `__password` is `None`
         - Compare `__password` and the MD5 value of `pwd`
         """
+		
+
         if pwd is None or type(pwd) is not str:
             return False
         if self.__password is None:
@@ -84,7 +86,7 @@ if __name__ == '__main__':
     if user_2.password is not None:
         print("User.password should be None if setter to an integer")
 
-    if not user_1.is_valid_password(u_pwd):
+    if user_1.is_valid_password(u_pwd):
         print("is_valid_password should return True if it's the right \
 password")
 
